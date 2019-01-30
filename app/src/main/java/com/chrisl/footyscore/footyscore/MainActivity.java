588abbf4361c6c2457f8b5a9a4642a0c143f7e05
Pherlar <christopherlarmour@gmail.com>
2019-01-30 16:07:52 +1100

    Change layout to ConstraintLayout

--- app/src/main/java/com/chrisl/footyscore/footyscore/MainActivity.java
+++ app/src/main/java/com/chrisl/footyscore/footyscore/MainActivity.java
@@ -12,8 +12,6 @@ public class MainActivity extends AppCompatActivity
     int teamAbehinds = 0;
     int teamBgoals = 0;
     int teamBbehinds = 0;
-    int teamAscore = 0;
-    int teamBscore = 0;
 
 
 
@@ -34,69 +32,68 @@ public class MainActivity extends AppCompatActivity
         teamAbehinds = 0;
         teamBgoals = 0;
         teamBbehinds = 0;
-        teamAscore = 0;
-        teamBscore = 0;
-        displayForTeamBscoreL1();
-        displayForTeamBscoreL2();
-        displayForTeamAscoreL1();
-        displayForTeamAscoreL2();
+        updateScore();
+
     }
 
 
 
     //Displays the Line 1  (goals and behinds) Score for Team A
 
-    public void displayForTeamAscoreL1 (){
-        TextView teamAScoreLine1 = findViewById(R.id.teamAScoreLine1);
-        teamAScoreLine1.setText(String.valueOf(teamAbehinds)+ " . " +String.valueOf(teamAgoals));
-    }
+//    public void displayForTeamAscoreL1 (){
+//        TextView teamAScoreLine1 = findViewById(R.id.teamAScoreLine1);
+//        teamAScoreLine1.setText(String.valueOf(teamAbehinds)+ " . " +String.valueOf(teamAgoals));
+//    }
 
     //Displays the Line 2 (total) Score for Team A
-    public void displayForTeamAscoreL2 (){
+    public void updateScore (){
         TextView teamAScoreLine2 = findViewById(R.id.teamAScoreLine2);
         teamAScoreLine2.setText(String.valueOf(teamAgoals * 6 + teamAbehinds));
+        TextView teamAScoreLine1 = findViewById(R.id.teamAScoreLine1);
+        teamAScoreLine1.setText(String.valueOf(teamAbehinds)+ " . " +String.valueOf(teamAgoals));
+        TextView teamBScoreLine1 = findViewById(R.id.teamBScoreLine1);
+        teamBScoreLine1.setText(String.valueOf(teamBbehinds)+ " . " +String.valueOf(teamBgoals));
+        TextView teamBScoreLine2 = findViewById(R.id.teamBScoreLine2);
+        teamBScoreLine2.setText(String.valueOf(teamBgoals * 6 + teamBbehinds));
+
     }
 
     //Displays the Line 1  (goals and behinds) Score for Team B
 
-    public void displayForTeamBscoreL1 (){
-        TextView teamBScoreLine1 = findViewById(R.id.teamBScoreLine1);
-        teamBScoreLine1.setText(String.valueOf(teamBbehinds)+ " . " +String.valueOf(teamBgoals));
-    }
+//    public void displayForTeamBscoreL1 (){
+//        TextView teamBScoreLine1 = findViewById(R.id.teamBScoreLine1);
+//        teamBScoreLine1.setText(String.valueOf(teamBbehinds)+ " . " +String.valueOf(teamBgoals));
+//    }
 
 
     //Displays the Line 2 (total) Score for Team B
-    public void displayForTeamBscoreL2 (){
-        TextView teamBScoreLine2 = findViewById(R.id.teamBScoreLine2);
-        teamBScoreLine2.setText(String.valueOf(teamBgoals * 6 + teamBbehinds));
-    }
+//    public void displayForTeamBscoreL2 (){
+//        TextView teamBScoreLine2 = findViewById(R.id.teamBScoreLine2);
+//        teamBScoreLine2.setText(String.valueOf(teamBgoals * 6 + teamBbehinds));
+//    }
 
     //runs on click of button teamAGoal
     public void teamAGoalButton(View view){
         teamAgoals = teamAgoals + 1;
-        displayForTeamAscoreL1();
-        displayForTeamAscoreL2();
-    }
+        updateScore();
+            }
 
     //runs on click of button teamAbehind
     public void teamAbehindButton(View view){
         teamAbehinds = teamAbehinds + 1;
-        displayForTeamAscoreL1();
-        displayForTeamAscoreL2();
+        updateScore();
     }
 
     //runs on click of button teamBGoal
     public void teamBgoalButton(View view){
         teamBgoals = teamBgoals + 1;
-        displayForTeamBscoreL1();
-        displayForTeamBscoreL2();
+        updateScore();
     }
 
     //runs on click of button teamBbehind
     public void teamBbehindButton(View view){
         teamBbehinds = teamBbehinds + 1;
-        displayForTeamBscoreL1();
-        displayForTeamBscoreL2();
+        updateScore();
     }
 
     //runs on click of resetScores
